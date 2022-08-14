e1ee1b88276330b602ae52b5e90a00720117148c
taranakitom <tomburgess@slingshot.co.nz>
2022-08-14 15:49:44 +1200

    Update Blackjack.py
    - making the program functional with the new system (it's not I can't figure out how to remove the right values)

--- Blackjack.py
+++ Blackjack.py
@@ -13,10 +13,16 @@ human_cards = []
 
 def template_to_cards():
     for i in range(deck_amount):
-        computer_cards.append(computer_cards_template)
-        human_cards.append(human_cards_template)
+        computer_cards.extend(computer_cards_template)
+        human_cards.extend(human_cards_template)
 
-def blackjack(balance):
+def print_cards(dealer_cards, user_cards):
+    print(f"\nDealer's cards:\n{dealer_cards[0]} _\nYour cards:\n{' '.join(map(str, user_cards))}")
+
+def print_cards_real(dealer_cards, user_cards):
+    print(f"\nDealer's cards:\n{' '.join(map(str, dealer_cards))}\nYour cards:\n{' '.join(map(user_cards))}")
+
+def blackjack(balance, difficulty):
     # define balance and bet
     if difficulty == 3:
         print(f"Your balance is {balance}")
@@ -25,15 +31,26 @@ def blackjack(balance):
         bet = 0
 
     # generate the values for the computer
-    user_card1 = computer_cards[random.randint(1, len(computer_cards))]
-    user_card2 = computer_cards[random.randint(1, len(computer_cards))]
-    dealer_card1 = computer_cards[random.randint(1, len(computer_cards))]
-    dealer_card2 = computer_cards[random.randint(1, len(computer_cards))]
+    user_card1 = computer_cards[random.randint(0, len(computer_cards)) - 1]
+    computer_cards.remove(user_card1)
+    user_card2 = computer_cards[random.randint(0, len(computer_cards)) - 1]
+    computer_cards.remove(user_card2)
+    dealer_card1 = computer_cards[random.randint(0, len(computer_cards)) - 1]
+    computer_cards.remove(dealer_card1)
+    dealer_card2 = computer_cards[random.randint(0, len(computer_cards)) - 1]
+    computer_cards.remove(dealer_card2)
 
     # generate the cards to show the user
-    user_cards = f"{human_cards[computer_cards.index(user_card1)]} {human_cards[computer_cards.index(user_card2)]}"
-    dealer_cards = f"{human_cards[computer_cards.index(dealer_card1)]} {human_cards[computer_cards.index(dealer_card2)]}"
+    user_cards = [human_cards[computer_cards.index(user_card1)], human_cards[computer_cards.index(user_card2)]]
+    dealer_cards = [human_cards[computer_cards.index(dealer_card1)], human_cards[computer_cards.index(dealer_card2)]]
 
+    # remove used cards from decks
+    human_cards.remove(user_cards[0])
+    human_cards.remove(user_cards[1])
+    human_cards.remove(dealer_cards[0])
+    human_cards.remove(dealer_cards[1])
+
+    # calculate the sums for the user and dealer
     user_sum = user_card1 + user_card2
     dealer_sum = dealer_card1 + dealer_card2
 
@@ -41,13 +58,13 @@ def blackjack(balance):
     user_aces = 0
     dealer_aces = 0
 
-    if user_card1 == 1:
+    if user_card1 == 11:
         user_aces += 1
-    if user_card2 == 1:
+    if user_card2 == 11:
         user_aces += 1
-    if dealer_card1 == 1:
+    if dealer_card1 == 11:
         dealer_aces += 1
-    if dealer_card2 == 1:
+    if dealer_card2 == 11:
         dealer_aces += 1
 
     loop = True
@@ -55,34 +72,39 @@ def blackjack(balance):
     or_x2 = " or x2"
     bust = False
 
-    #  starting blackjack detection for user
+    print_cards(dealer_cards, user_cards)
+
+    # starting blackjack detection for user
     if user_sum == 21:
         loop = False
+        print_cards_real(dealer_cards, user_cards)
         if dealer_sum == 21:
             print("It's a push!")
         else:
             print("You got Blackjack!")
-            balance += bet * 1.5
+            balance += bet * 2.5
     
     # starting blackjack detection for dealer
     if dealer_sum == 21:
         loop = False
-        print(f"\nDealer's cards:\n{dealer_cards}\nUnfortunate, the dealer got Blackjack!")
+        print_cards_real(dealer_cards, user_cards)
+        print(f"\nUnfortunate, the dealer got Blackjack!")
         balance -= bet
 
     while loop is True:
-        print(f"\nDealer's cards:\n{into_card(dealer_card1)} _\nYour cards:\n{user_cards}")
-
         action = input(f"What would you like to do?\nYou can stand or hit{or_x2}\n-")
 
         # if the player hits
         if action == "hit":
-            new_card = random.randint(1, 13)
-            user_sum += to_real_value(new_card)
-            user_cards += f" {into_card(new_card)}"
+            new_card = computer_cards[random.randint(0, len(computer_cards)) - 1]
+            user_sum += new_card
+            user_cards.append(human_cards[computer_cards.index(new_card)])
+
+            human_cards.pop(user_cards.index(computer_cards.index(new_card)))
+            computer_cards.remove(new_card)
 
             # makes aces work dw about it
-            if new_card == 1:
+            if new_card == 11:
                 user_aces += 1
 
             if user_sum > 21:
@@ -96,12 +118,15 @@ def blackjack(balance):
         elif action == "x2":
             # ensures this can only be done on the first round
             if can_double is True:
-                new_card = random.randint(1, 13)
-                user_sum += to_real_value(new_card)
-                user_cards += f" {into_card(new_card)}"
+                new_card = computer_cards[random.randint(0, len(computer_cards)) - 1]
+                user_sum += new_card
+                user_cards.append(human_cards[computer_cards.index(new_card)])
+
+                human_cards.pop(user_cards.index(computer_cards.index(new_card)))
+                computer_cards.remove(new_card)
 
                 # makes aces work dw about it
-                if new_card == 1:
+                if new_card == 11:
                     user_aces += 1
 
                 if user_sum > 21:
@@ -111,20 +136,24 @@ def blackjack(balance):
                     else:
                         loop = False
 
-
                 bet += bet
 
                 loop = False
 
         # if the player stands
         elif action == "stand":
-            print(f"\nDealer's cards:\n{dealer_cards}")
+            print_cards_real(dealer_cards, user_cards)
             while dealer_sum < 17 and not bust:
-                new_card = random.randint(1, 13)
-                dealer_sum += to_real_value(new_card)
+                new_card = computer_cards[random.randint(0, len(computer_cards)) - 1]
+                dealer_sum += new_card
+                dealer_cards.append(human_cards[computer_cards.index(new_card)])
+                print_cards_real(dealer_cards, user_cards)
+
+                human_cards.pop(human_cards.index(computer_cards.index(new_card)))
+                computer_cards.remove(new_card)
 
                 # makes aces work dw about it
-                if new_card == 1:
+                if new_card == 11:
                     dealer_aces += 1
 
                 if dealer_sum > 21:
@@ -135,9 +164,7 @@ def blackjack(balance):
                         bust = True
                         dealer_sum = 0
 
-                dealer_cards += f" {into_card(new_card)}"
-                print(f"Dealer's cards:\n{dealer_cards}")
-
+                
             if user_sum == dealer_sum:
                 print("Interesting, a push!")
             if user_sum < dealer_sum:
@@ -160,7 +187,7 @@ def main():
     template_to_cards()
 
     if difficulty == 3:
-        balance = int(input("What is your balance?\n-"))
+        balance = int(input("What is your balance?\n>"))
     else:
         balance = 1
 
